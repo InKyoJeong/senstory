2542d32ca87da2c53f3994c7f2086d4e4db41846
InKyoJeong <viapolar@naver.com>
2021-07-08 23:27:21 +0900

    fix: Change infinite with intersection-observer

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { LOAD_POST_REQUEST } from "../actions/post";
+import { useInView } from "react-intersection-observer";
 
 import Layout from "../components/Layout";
 import PostCard from "../components/PostCard";
@@ -13,36 +14,43 @@ const Home = () => {
   const { mainPosts, hasMorePosts, loadPostLoading } = useSelector(
     (state) => state.post
   );
+  const [ref, inView] = useInView();
 
   useEffect(() => {
-    dispatch({
-      type: LOAD_POST_REQUEST,
-    });
-  }, []);
-
-  useEffect(() => {
-    function onScroll() {
-      console.log(
-        window.pageYOffset,
-        document.documentElement.clientHeight,
-        document.documentElement.scrollHeight
-      );
-      if (
-        window.pageYOffset + document.documentElement.clientHeight >
-        document.documentElement.scrollHeight - 400
-      ) {
-        if (hasMorePosts && !loadPostLoading) {
-          dispatch({
-            type: LOAD_POST_REQUEST,
-          });
-        }
-      }
+    if (inView && hasMorePosts && !loadPostLoading) {
+      const lastId = mainPosts[mainPosts.length - 1]?.id;
+      dispatch({
+        type: LOAD_POST_REQUEST,
+        lastId,
+      });
     }
-    window.addEventListener("scroll", onScroll);
-    return () => {
-      window.removeEventListener("scroll", onScroll);
-    };
-  }, [hasMorePosts, loadPostLoading]);
+  }, [inView, hasMorePosts, loadPostLoading, mainPosts]);
+
+  // useEffect(() => {
+  //   dispatch({
+  //     type: LOAD_POST_REQUEST,
+  //   });
+  // }, []);
+
+  // useEffect(() => {
+  //   function onScroll() {
+  //     if (
+  //       window.scrollY + document.documentElement.clientHeight >
+  //       document.documentElement.scrollHeight - 300
+  //     ) {
+  //       if (hasMorePosts && !loadPostLoading) {
+  //         dispatch({
+  //           type: LOAD_POST_REQUEST,
+  //         });
+  //       }
+  //     }
+  //   }
+  //   window.addEventListener("scroll", onScroll);
+
+  //   return () => {
+  //     window.removeEventListener("scroll", onScroll);
+  //   };
+  // }, [hasMorePosts, loadPostLoading]);
 
   return (
     <Layout>
@@ -53,6 +61,7 @@ const Home = () => {
       {mainPosts.map((post) => (
         <PostCard key={post.id} post={post} />
       ))}
+      <div ref={hasMorePosts && !loadPostLoading ? ref : undefined} />
     </Layout>
   );
 };
