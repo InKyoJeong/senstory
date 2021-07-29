a7050d8dc7023057886a1670ae8a027a97aed13e
InKyoJeong <viapolar@naver.com>
2021-07-29 21:20:51 +0900

    refactor: Change sequelize model to class

--- back/models/user.js
+++ back/models/user.js
@@ -1,32 +1,38 @@
-module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define(
-    "User",
-    {
-      email: {
-        type: DataTypes.STRING(35),
-        allowNull: false,
-        unique: true,
-      },
-      nickname: {
-        type: DataTypes.STRING(30),
-        allowNull: false,
-      },
-      intro: {
-        type: DataTypes.STRING(100),
-        allowNull: true,
-      },
-      password: {
-        type: DataTypes.STRING(100),
-        allowNull: false,
-      },
-    },
-    {
-      charset: "utf8",
-      collate: "utf8_general_ci",
-    }
-  );
+const DataTypes = require("sequelize");
+const { Model } = DataTypes;
 
-  User.associate = (db) => {
+module.exports = class User extends Model {
+  static init(sequelize) {
+    return super.init(
+      {
+        email: {
+          type: DataTypes.STRING(35),
+          allowNull: false,
+          unique: true,
+        },
+        nickname: {
+          type: DataTypes.STRING(30),
+          allowNull: false,
+        },
+        intro: {
+          type: DataTypes.STRING(100),
+          allowNull: true,
+        },
+        password: {
+          type: DataTypes.STRING(100),
+          allowNull: false,
+        },
+      },
+      {
+        modelName: "User",
+        tableName: "users",
+        charset: "utf8",
+        collate: "utf8_general_ci",
+        sequelize,
+      }
+    );
+  }
+  static associate(db) {
     db.User.hasMany(db.Post);
     db.User.hasMany(db.Comment);
     db.User.belongsToMany(db.Post, { through: "Like", as: "Liked" });
@@ -40,7 +46,5 @@ module.exports = (sequelize, DataTypes) => {
       as: "Followings",
       foreignKey: "FollowerId",
     });
-  };
-
-  return User;
+  }
 };
