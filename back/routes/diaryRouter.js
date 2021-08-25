d70f6324a8370ff8b8d5fd379fcea383ae59e34d
InKyoJeong <viapolar@naver.com>
2021-08-25 17:42:52 +0900

    refactor: Remove unnecessary package

--- back/routes/diaryRouter.js
+++ back/routes/diaryRouter.js
@@ -1,7 +1,4 @@
 const express = require("express");
-const multer = require("multer");
-const path = require("path");
-const fs = require("fs");
 const { isLoggedIn, errorHandler } = require("./middlewares");
 const { upload } = require("../utils/index.js");
 const {
