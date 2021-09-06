553ffbc95d936808e8032f5357867ac0f92b13db
InKyoJeong <viapolar@naver.com>
2021-09-06 12:12:16 +0900

    fix: Fix aws upload

--- back/routes/postRouter.js
+++ back/routes/postRouter.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+const multerS3 = require("multer-s3");
+const AWS = require("aws-sdk");
+
 const { isLoggedIn, errorHandler } = require("./middlewares");
-const { upload } = require("../utils/index.js");
+// const { upload } = require("../utils/index.js");
 const {
   postAddPost,
   postImages,
@@ -14,6 +20,29 @@ const {
 } = require("../controller/postController");
 
 const postRouter = express.Router();
+
+try {
+  fs.accessSync("uploads");
+} catch (error) {
+  fs.mkdirSync("uploads");
+}
+
+AWS.config.update({
+  accessKeyId: process.env.S3_ACCESS_KEY_ID,
+  secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+  region: "ap-northeast-2",
+});
+
+const upload = multer({
+  storage: multerS3({
+    s3: new AWS.S3(),
+    bucket: "senstory",
+    key(req, file, cb) {
+      cb(null, `original/${Date.now()}_${path.basename(file.originalname)}`);
+    },
+  }),
+  limits: { fileSize: 20 * 1024 * 1024 }, // 20MB 제한
+});
 
 postRouter.post("/", isLoggedIn, upload.none(), postAddPost, errorHandler);
 postRouter.post("/images", isLoggedIn, upload.array("image"), postImages);
